e92a6008b55ec117cdb08fe0f7ea71993d038975
Yiming Peng <91288087+yimipeng@users.noreply.github.com>
2022-02-10 21:15:26 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import boto3
 from flask import Flask
+from __future__ import print_function
 import os
 import time
 import json 
 import requests
+import decimal
 
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
@@ -25,7 +27,20 @@ from opentelemetry.sdk.extension.aws.trace.propagation.aws_xray_format import (
     TRACE_ID_VERSION,
 )
 
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
 
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+    
 otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317")
 span_processor = BatchSpanProcessor(otlp_exporter)
 trace.set_tracer_provider(TracerProvider(active_span_processor=span_processor, id_generator=AwsXRayIdGenerator()))
@@ -40,6 +55,12 @@ RequestsInstrumentor().instrument()
 # Initialize `Instrumentor` for the `flask` web framework
 FlaskInstrumentor().instrument_app(app)
 
+
+dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
+
+table = dynamodb.Table('ProductCatalog')
+
+
 def convert_otel_trace_id_to_xray(otel_trace_id_decimal):
     otel_trace_id_hex = "{:032x}".format(otel_trace_id_decimal)
     x_ray_trace_id = TRACE_ID_DELIMITER.join(
@@ -51,9 +72,21 @@ def convert_otel_trace_id_to_xray(otel_trace_id_decimal):
     )
     return '{{"traceId": "{}"}}'.format(x_ray_trace_id)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello World! App Runner'
+    try:
+        response = table.get_item(
+            Key={
+                'pk': "0"
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("GetItem succeeded:")
+        print(json.dumps(response['Item'], indent=4, cls=DecimalEncoder))
+    return 'Thanks for using AWS App Runner!'
 @app.route('/health')
 def health_check():
     return 'health check'
